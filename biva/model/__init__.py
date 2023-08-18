955fb9e0d7aaae34716724d4889e419d7a3bd418
rkharkness <scrha@leeds.ac.uk>
2023-08-18 15:46:26 +0100

    Add MOOD preprocessing fn. Update MOOD dataloading.

--- biva/model/__init__.py
+++ biva/model/__init__.py
@@ -1,4 +1,4 @@
-from .architectures import get_deep_vae_mnist, get_deep_vae_cifar
+from .architectures import get_deep_vae_mnist, get_deep_vae_cifar, get_deep_vae_brain, get_deep_vae_abdom
 from .deepvae import DeepVae, VAE, LVAE, BIVA
 from .stage import BaseStage, VaeStage, LvaeStage, BivaStage
 from .stochastic import StochasticLayer, DenseNormal, ConvNormal
