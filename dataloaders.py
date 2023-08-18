cd4991a8030500227a8cb6ecd6034cab87c07956
rkharkness <scrha@leeds.ac.uk>
2023-08-18 15:45:22 +0100

    Fix dataloader selection.

--- dataloaders.py
+++ dataloaders.py
@@ -5,27 +5,27 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
 def get_dataloaders(data, path, bs = 64, num_workers = 4):
-    if data == 'binmnist':
+    if 'binmnist' in data:
         train_dataset, valid_dataset, test_dataset = get_binmnist_datasets(path)
 
-    elif data == 'cifar10':
+    elif 'cifar' in data:
         from transforms import Lambda
 
         transform = Lambda(lambda x: x * 2 - 1)
         train_dataset, valid_dataset, test_dataset = get_cifar10_datasets(path, transform=transform)
 
-    elif data == 'mood':
+    elif 'mood' in data:
         train_loader = get_numpy2d_dataset(path, batch_size=bs)
         valid_loader = get_numpy2d_dataset(path, batch_size=bs, mode="val")
         test_loader = valid_loader
 
-    elif data == 'luna16':
+    elif 'luna16' in data:
         raise NotImplementedError
     
     else:
         raise NotImplementedError
 
-    if data != 'mood':
+    if 'mood' not in data:
         train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True, pin_memory=False, num_workers=num_workers)
         valid_loader = DataLoader(valid_dataset, batch_size=2 * bs, shuffle=True, pin_memory=False,
                                 num_workers=num_workers)
